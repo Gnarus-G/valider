15d0ebc645da838847326d28a12c44425b0886a6
gnarus-g <g.avet.gnaritas@gmail.com>
2022-02-02 01:04:02 -0500

    poc done, a likely better way

--- index.ts
+++ index.ts
@@ -1,4 +1,11 @@
-import { Errors, minLength, required, validate } from "./validators";
+import {
+  DataValidator,
+  ErrorsCollectors,
+  minLength,
+  required,
+  validate,
+  validateValue,
+} from "./validators";
 
 const data = {
   username: "",
@@ -7,7 +14,7 @@ const data = {
 };
 
 const withValidators =
-  <T>(data: T, validators: Array<(data: T) => Errors<T>>) =>
+  <T>(data: T, validators: Array<DataValidator<T>>) =>
   () =>
     validators
       .map((validate) => validate(data))
@@ -16,10 +23,46 @@ const withValidators =
         return acc;
       }, {});
 
+const collectErrors =
+  <T>(data: T, errorsCollector: ErrorsCollectors<T>) =>
+  () =>
+    Object.entries<any>(errorsCollector)
+      .map(([key, validators]) => ({
+        [key]: Array.isArray(validators)
+          ? validators.map((validate) => validate(data[key]))
+          : validators(data[key]),
+      }))
+      .reduce((acc, partialError) => {
+        Object.assign(acc, partialError);
+        return acc;
+      }, {});
+
 let validateData = withValidators(data, [
   required(["username", "password", "date"], "needed"),
   minLength(["username", "password", "date"], 8, "at least 8 characters"),
-  validate("date", "before now", (date: Date) => date.getTime() < Date.now() + 1),
+  validate(
+    "date",
+    "should be before now",
+    (date: Date) => date.getTime() < Date.now()
+  ),
 ]);
+
+console.log(validateData());
+
+const isRequired = validateValue((value) => !!value, "required");
+const isBeforeNow = validateValue(
+  (value: Date) => value.getTime() < Date.now(),
+  "should be before now"
+);
+
+validateData = collectErrors(data, {
+  username: [isRequired],
+  password: isRequired,
+  date: [
+    isRequired,
+    validateValue((date) => date.getTime() > Date.now(), "should be after now"),
+    isBeforeNow,
+  ],
+});
 
 console.log(validateData());
--- validators.ts
+++ validators.ts
@@ -1,7 +1,14 @@
-export type Errors<T> = Partial<Record<keyof T, string | true>>;
-export type Validator<T> = (data: T) => Errors<T>;
+type ValidationState = string | boolean;
+export type ValidatedValues<T> = Partial<Record<keyof T, ValidationState>>;
+export type DataValidator<T> = (data: T) => ValidatedValues<T>;
+export type ValueValidator<T> = (value: T) => ValidationState;
+export type ErrorsCollectors<Data> = {
+  [Key in keyof Data]:
+    | ValueValidator<Data[Key]>
+    | Array<ValueValidator<Data[Key]>>;
+};
 type Keys<T> = keyof T | Array<keyof T>;
-type Predicate = (value: any) => boolean;
+type Predicate<T = any> = (value: T) => boolean;
 
 export const required = <T>(key: Keys<T>, msg?: string) =>
   validate(key, msg, (value) => !!value);
@@ -10,7 +17,11 @@ export const minLength = <T>(key: Keys<T>, min: number, msg: string) =>
   validate(key, msg, (value: string) => value.length >= min);
 
 export const validate =
-  <T>(keyOrKeys: Keys<T>, msg: string, predicate: Predicate): Validator<T> =>
+  <T>(
+    keyOrKeys: Keys<T>,
+    msg: string,
+    predicate: Predicate
+  ): DataValidator<T> =>
   (data) => {
     if (Array.isArray(keyOrKeys))
       return keyOrKeys
@@ -18,9 +29,15 @@ export const validate =
         .reduce((acc, k) => {
           acc[k] = msg || true;
           return acc;
-        }, {} as Errors<T>);
+        }, {} as ValidatedValues<T>);
 
     return {
       [keyOrKeys]: predicate(data[keyOrKeys]) || msg || true,
-    } as Errors<T>;
+    } as ValidatedValues<T>;
+  };
+
+export const validateValue =
+  <T>(predicate: Predicate<T>, msg?: string) =>
+  (value: T) => {
+    return predicate(value) || msg;
   };
