a64f342c270ab87b3b4000be17fd45f1ca8394ca
gnarus-g <g.avet.gnaritas@gmail.com>
2022-02-02 22:53:39 -0500

    tsconfigs setup

--- packages/core/core.ts
+++ packages/core/core.ts
@@ -1,17 +1,15 @@
 type ValidationState = string | boolean;
 type ValueValidator<T> = (value: T) => ValidationState;
+type ValueValidators<T> = ValueValidator<T> | Array<ValueValidator<T>>;
 type ValidationStatesCollector<Data> = {
-  [Key in keyof Data]?:
-    | ValueValidator<Data[Key]>
-    | Array<ValueValidator<Data[Key]>>;
+  [Key in keyof Data]?: ValueValidators<Data[Key]>;
 };
 type Predicate<T = any> = (value: T) => boolean;
 
 export const validate =
-  <T>(predicate: Predicate<T>, msg?: string): ValueValidator<T> =>
-  (value: T) => {
-    return predicate(value) || msg;
-  };
+  <T>(predicate: Predicate<T>, msg: string): ValueValidator<T> =>
+  (value) =>
+    predicate(value) || msg;
 
 export const collectValidationState =
   <T>(data: T, errorsCollector: ValidationStatesCollector<T>) =>
@@ -19,8 +17,8 @@ export const collectValidationState =
     Object.entries<any>(errorsCollector)
       .map(([key, validators]) => ({
         [key]: Array.isArray(validators)
-          ? validators.map((validate) => validate(data[key]))
-          : validators(data[key]),
+          ? validators.map((validate) => validate(data[key as keyof T]))
+          : validators(data[key as keyof T]),
       }))
       .reduce((acc, partialError) => {
         Object.assign(acc, partialError);
