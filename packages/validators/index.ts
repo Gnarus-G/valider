00d512c59a6206fc95937d677c17123e6d1d7dad
gnarus-g <g.avet.gnaritas@gmail.com>
2022-02-05 19:22:44 -0500

    re-exporting at index

---
+++ packages/validators/index.ts
@@ -0,0 +1,5 @@
+export { default as isAfterNow } from "./src/isAfterNow";
+export { default as isBeforeNow } from "./src/isBeforeNow";
+export { default as required } from "./src/required";
+export { default as minLength } from "./src/minLength";
+export { default as isEmail } from "./src/isEmail";
